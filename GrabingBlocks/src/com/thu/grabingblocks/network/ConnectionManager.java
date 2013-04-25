76277bfc1df96f2df0810109a22808db837b5647
Yang Yang <geraint0923@gmail.com>
2013-04-26 01:06:26 +0800

    Using singleton for ConnectionManager

--- GrabingBlocks/src/com/thu/grabingblocks/network/ConnectionManager.java
+++ GrabingBlocks/src/com/thu/grabingblocks/network/ConnectionManager.java
@@ -4,5 +4,16 @@ package com.thu.grabingblocks.network;
 
 
 public class ConnectionManager {
+	private ConnectionManager() {
+		
+	}
 	
+	private static ConnectionManager instance;
+	
+	public ConnectionManager getInstance(){
+		if(instance == null) {
+			instance = new ConnectionManager();
+		}
+		return instance;
+	}
 }
